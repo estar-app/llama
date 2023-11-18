e0fda4f8c8dad05358c8dccbf7a5aef6fc37f2d3
Fortune Ebarim <fortuneebarim@gmail.com>
2023-11-18 09:54:32 +0000

    update

--- llama/generation.py
+++ llama/generation.py
@@ -16,8 +16,8 @@ from fairscale.nn.model_parallel.initialize import (
     model_parallel_is_initialized,
 )
 
-from llama.model import ModelArgs, Transformer
-from llama.tokenizer import Tokenizer
+from llama.llama.model import ModelArgs, Transformer
+from llama.llama.tokenizer import Tokenizer
 
 Role = Literal["system", "user", "assistant"]
 
